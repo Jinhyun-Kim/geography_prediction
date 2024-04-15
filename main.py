f1f610345950df49bc1391389b37a679e98e09f2
Jinhyun Kim <jinhyun9130@gmail.com>
2024-04-15 18:47:48 +0900

    - Fully implemented all feature selection method.\n - Save PCA, tSNE, confusion_matrix as images.\n - Print out per class accuraces to excel file.

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import os, time
 from tqdm import tqdm
 import pandas as pd
-import numpy as np # type: ignore
-import pickle
+import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import seaborn as sns
 
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.feature_selection import SelectKBest, chi2, f_classif, f_regression, r_regression, mutual_info_classif
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix #, roc_curve, roc_auc_score, recall_score, precision_score,
-from sklearn.preprocessing import LabelEncoder
-# from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, chi2, f_classif, mutual_info_classif, SequentialFeatureSelector
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_recall_fscore_support #, roc_curve, roc_auc_score, recall_score, precision_score,
+from sklearn.preprocessing import LabelEncoder#, StandardScaler
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier #,GradientBoostingClassifier
-from xgboost import XGBClassifier
-
+from sklearn.ensemble import RandomForestClassifier #, ExtraTreesClassifier,GradientBoostingClassifier
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+
+from xgboost import XGBClassifier
 
 from helpers import get_ip_address, has_write_permission, measure_performance
 
@@ -46,6 +43,7 @@ logger.addHandler(log_file_handler)
 # logger.addHandler(log_console_handler)
 
 RANDOM_SEED = 42
+RANDOM_SEED_DATASET = 42
 
 class data_loader:
     def __init__(self, X_path, sample_annotation_file):
@@ -60,7 +58,7 @@ class data_loader:
         logging.info(f"[progress] Read data done. X.shape: {self.X.shape}, y.shape: {self.y.shape}")
 
         self.drop_notusing_sample()
-        self.y_encoded = self.encode_y()
+        self.y_encoded, self.label_mapping = self.encode_y()
         self.train_indices, self.test_indices = self.split_dataset()
 
         assert self.X.shape[0] == self.y.shape[0]
@@ -79,16 +77,18 @@ class data_loader:
     def encode_y(self):
         label_encoder = LabelEncoder()
         y_encoded = label_encoder.fit_transform(self.y)
-        return(y_encoded)
+        label_mapping = dict(zip(label_encoder.transform(label_encoder.classes_), label_encoder.classes_))
+
+        return y_encoded, label_mapping
 
     def split_dataset(self):
-        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state = RANDOM_SEED)
+        sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state = RANDOM_SEED_DATASET)
         train_indices, test_indices = next(sss.split(self.X, self.y))
         return(train_indices, test_indices)
 
 
     def get_data(self):
-        return self.X, self.y_encoded, self.train_indices, self.test_indices
+        return self.X, np.array(self.y), self.y_encoded, self.train_indices, self.test_indices, self.label_mapping
         
     def get_combined_df(self):
         df = pd.DataFrame(self.X)
@@ -114,9 +114,8 @@ def train_ML(X_train, y_train, X_test, method = "SVM"):#["SVM", "XGB", "DT"]:
             # 'n_estimators': 100,
             'gamma': 0, # default
             'subsample': 1, # default
-            # 'random_state': random_seed
         }
-        xgboost_model = XGBClassifier(**xgboost_params)
+        xgboost_model = XGBClassifier(**xgboost_params, random_state = RANDOM_SEED)
         xgboost_model.fit(X_train, y_train)
         y_pred = xgboost_model.predict(X_test)
 
@@ -127,19 +126,32 @@ def train_ML(X_train, y_train, X_test, method = "SVM"):#["SVM", "XGB", "DT"]:
 
     return y_pred
 
-def evaluate_performance(y_test, y_pred):
+def evaluate_performance(y_test, y_pred, label_mapping, save_file_previx):
     accuracy = accuracy_score(y_test, y_pred)
     f1_micro = f1_score(y_test, y_pred, average='micro')
     f1_macro = f1_score(y_test, y_pred, average='macro')
     f1_weighted = f1_score(y_test, y_pred, average='weighted')
 
     conf_matrix = confusion_matrix(y_test, y_pred)
-    plt.figure(figsize=(8, 6))
+
+    precisions, recalls, f1_scores, _ = precision_recall_fscore_support(y_test, y_pred, average=None)
+    
+    class_names = [label_mapping.get(i, f"Class_{i}") for i in range(len(np.unique(y_test)))]
+    class_accuracies = conf_matrix.diagonal() / conf_matrix.sum(axis=1)
+
+    # Creating a dictionary for class-specific metrics
+    class_metrics = {
+        f"class_{class_name}_accuracy": acc for class_name, precision, recall, f1, acc in zip(class_names, precisions, recalls, f1_scores, class_accuracies)
+    }
+
+    plt.figure(figsize=(8, 7))
     sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False, 
                 xticklabels=['Class 0', 'Class 1'], yticklabels=['Class 0', 'Class 1'])
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.title('Confusion Matrix')
+
+    plt.savefig(f"{save_file_previx}_confusion_matrix.pdf", dpi = 300)
     plt.show()
 
     metrics = {
@@ -147,17 +159,12 @@ def evaluate_performance(y_test, y_pred):
         'f1_micro': f1_micro,
         'f1_macro': f1_macro,
         'f1_weighted': f1_weighted,
-        'confusion_matrix': conf_matrix  
+        'confusion_matrix': conf_matrix,
+        **class_metrics
     }
 
     return metrics
 
-def uni_feature_selection(X, y, score_func, n):
-    print(f"input array shape : {X.shape}, {y.shape}. n = {n}")
-    X_selected = SelectKBest(score_func, k = n).fit_transform(X, y)
-    print(f"output array shape : {X_selected.shape}")
-
-    return(X_selected)
 
 @measure_performance
 def select_feature(X, y, method, n, train_idx): 
@@ -165,38 +172,30 @@ def select_feature(X, y, method, n, train_idx):
         X_train = X[train_idx]
         y_train = y[train_idx]
 
-        clf = RandomForestClassifier(n_estimators=1000)
+        clf = RandomForestClassifier(n_estimators=1000, random_state = RANDOM_SEED)
         clf.fit(X_train, y_train)
 
         fi = clf.feature_importances_
-
         fi_series = pd.Series(fi)
         selected_indices = fi_series.nlargest(n).index
         
         X_selected = X[:, selected_indices]
 
-
     else:
-        np.random.seed(RANDOM_SEED)
         num_snps_before = X.shape[1]
 
         if method in ["random", "variance"]:
             if method == "random":
+                rng = np.random.default_rng(seed = RANDOM_SEED)
                 boolean_mask = np.zeros(num_snps_before, dtype=bool)
-                selected_indices = np.random.choice(num_snps_before, n, replace=False)
+                selected_indices = rng.choice(num_snps_before, n, replace=False)
                 boolean_mask[selected_indices] = True
 
             elif method == "variance":
-                threshold = 0.1
                 batch_process = False
-                # from sklearn.feature_selection import VarianceThreshold
-                # selector = VarianceThreshold(threshold = threshold)
-                # genotype_array_filtered = selector.fit_transform(genotype_array)
-                # boolean_mask = selector.get_support()
-                # print(f"This filter will return {genotype_array_filtered.shape} /", genotype_array.shape[1], f"variants (")
-                # print(f"This filter will retain {boolean_mask.sum()} /", genotype_array.shape[1], f"variants (", boolean_mask.sum()/genotype_array.shape[1] * 100,"%)")
 
                 if batch_process:
+                    raise NotImplemented
                     batch_size=100000
                     n_samples, n_snps = X.shape
                     variances = np.zeros((n_snps, feature_dim))
@@ -206,17 +205,11 @@ def select_feature(X, y, method, n, train_idx):
                         batch_var = np.var(genotype_array_onehot[:, start:end, :], axis=0)
                         variances[start:end, :] = batch_var
                 else:
-                    variances = np.var(genotype_array_onehot, axis=0)
+                    variances = np.var(X, axis=0)
                 
-                boolean_mask = (variances > threshold).any(axis=1)
-                print(f"Variance filter (threshold = {threshold}) will retain {boolean_mask.sum()} /", genotype_array_onehot.shape[1], f"variants (", boolean_mask.sum()/genotype_array_onehot.shape[1] * 100,"%)")
-
-                # sns.histplot(variances.reshape(-1), bins=100, kde=True)  # 'bins' controls the number of bins, 'kde' adds a Kernel Density Estimate plot
-                # plt.title('Histogram of Data')
-                # plt.xlabel('Variances')
-                # plt.ylabel('Frequency')
-                # plt.show()
-                raise(NotImplemented)
+                selected_indices = np.argsort(variances)[-n:]
+                boolean_mask = np.zeros(num_snps_before, dtype=bool)
+                boolean_mask[selected_indices] = True
 
             X_selected = X[:, boolean_mask]
             num_snps_after = X_selected.shape[1]
@@ -234,20 +227,17 @@ def select_feature(X, y, method, n, train_idx):
             else:
                 raise
 
-            X_selected = uni_feature_selection(X = snp_dataset.genotype_array, 
-                                               y = sample_annotation_df["Population code"],
-                                               score_func = score_fun,
-                                               n = n)
+            X_selected = SelectKBest(score_fun, k = n).fit_transform(X, y)
 
         elif method == "recursive_feature_selection":
-            print(f"input array shape : {X.shape}, {y.shape}. n = {n}")
             knn = KNeighborsClassifier(n_neighbors = n)
             sfs = SequentialFeatureSelector(knn, n_features_to_select = n)
             sfs.fit(X, y)
 
             #sfs.get_support()
             X_selected = sfs.transform(X)
-            print(f"output array shape : {X_selected.shape}")
+        else:
+            raise ValueError(f"Unsupported method: {method}")
 
     return(X_selected)
 
@@ -255,7 +245,7 @@ def draw_PCA(X, y, file_name):
     pca = PCA(n_components=2)
     pca_result = pca.fit_transform(X)
 
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 8))
 
     for label in np.unique(y):
         indices = np.where(y == label)
@@ -263,35 +253,42 @@ def draw_PCA(X, y, file_name):
     plt.title(f'PCA of {X.shape[1]} SNPs')
     plt.xlabel('PC1')
     plt.ylabel('PC2')
-    plt.legend(loc='best', prop={'size': 10})
+    #plt.legend(loc='best', prop={'size': 10})
+    plt.legend(loc=1, prop={'size': 5})
 
-    plt.savefig(f"{file_name}_PCA.png")
+    plt.savefig(f"{file_name}_PCA.pdf", dpi = 300)
     plt.show()  # Optionally show the plot
 
 def draw_tSNE(X, y, file_name):
     tsne = TSNE(n_components=2, verbose=1)
     tsne_result = tsne.fit_transform(X)
 
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(8, 8))
 
-    labels_unique = np.unique(y)
-    colors = cm.viridis(np.linspace(0, 1, len(labels_unique)))  # Using viridis colormap
-
-    for label, color in zip(labels_unique, colors):
+    for label in np.unique(y):
         indices = np.where(y == label)
-        plt.scatter(tsne_result[indices, 0], tsne_result[indices, 1], label=label, alpha=0.3,
-                    color=color,
-                    ) 
+        plt.scatter(tsne_result[indices, 0], tsne_result[indices, 1], label=label, alpha=0.5)
+
+    ## use other color codes
+    # labels_unique = np.unique(y)
+    # colors = cm.viridis(np.linspace(0, 1, len(labels_unique)))  # Using viridis colormap
+    # for label, color in zip(labels_unique, colors):
+    #     indices = np.where(y == label)
+    #     plt.scatter(tsne_result[indices, 0], tsne_result[indices, 1], label=label, alpha=0.3,
+    #                 color=color,
+    #                 ) 
 
     plt.title(f't-SNE of {X.shape[1]} SNPs')
     plt.xlabel('t-SNE 1')
     plt.ylabel('t-SNE 2')
-    plt.legend(loc='best', prop={'size': 10})
+    # plt.legend(loc='best', prop={'size': 10})
+    plt.legend(loc=1, prop={'size': 5})
 
-    plt.savefig(f"{file_name}_tSNE.png")
+    plt.savefig(f"{file_name}_tSNE.pdf", dpi = 300)
     plt.show()  # Optionally show the plot
 
-def get_data_path():
+
+def get_data_path(save_data_path):
     data_locations = {
         '223.195.111.48': '/project/datacamp/team11/data',
         '147.47.44.229': '/home/jinhyun/data/1kGP',
@@ -301,6 +298,9 @@ def get_data_path():
     sample_annotation_file = os.path.join(raw_data_path, "igsr-1000 genomes 30x on grch38.tsv")
     preprocess_path = os.path.join(raw_data_path, "preprocessed")
 
+    if not os.path.exists(save_data_path):
+        os.makedirs(save_data_path)
+
     assert os.path.exists(preprocess_path), f"Data path not exists: {raw_data_path} OR IP setting is incorrect: {get_ip_address()}"
     assert os.path.isfile(sample_annotation_file), f"File not exists : {sample_annotation_file}"
     # assert has_write_permission(preprocess_path), f"You do not have write permission for {preprocess_path}"
@@ -309,22 +309,28 @@ def get_data_path():
     
 
 def main():
-    # target_feature = "merged_support3_variance_0.1" # Real_data
-    target_feature = "merged_support3_variance_0.2499999" # Test_data
+    ### arguments -----
+    target_feature = "merged_support3_variance_0.1" # Real_data
+    # target_feature = "merged_random_1k" # Test_data
+    save_data_path = "./results"
 
-    feature_data_path, sample_annotation_file = get_data_path()
+    n_select_start = 128
+    select_methods = ["rf", "random", "variance", "chi2", "f_classif", "mutual_info_classif"]
+
+    ML_models = ["SVM", "XGB", "DT"]
+
+    ### code start -----
+    feature_data_path, sample_annotation_file = get_data_path(save_data_path)
 
     dataset = data_loader(os.path.join(feature_data_path, target_feature + "_matrix.npy"), 
                           sample_annotation_file)
-    X, y, train_indices, test_indices = dataset.get_data()
-    # df = dataset.get_combined_df()
+    X, y_original, y, train_indices, test_indices, label_mapping = dataset.get_data()
 
-    n_select_start = 128
     n_select_max_power = int(np.floor(np.log2(X.shape[1])))
     n_select_start_power = int(np.ceil(np.log2(n_select_start)))  
 
     result_combined = []
-    for feature_select_method in ["rf"]:#["random", "rf", "variance", "chi2", "f_classif", "mutual_info_classif"]:
+    for feature_select_method in select_methods:
         for power in range(n_select_start_power, n_select_max_power + 2):
             n_select = 2**power if (power <= n_select_max_power) else X.shape[1]
 
@@ -333,33 +339,34 @@ def main():
             logging.info(f"*************** current loop: {current_loop} ***************")
 
             try:
-                X_selected, perf_metric_select = select_feature(X = X, y = y, method = feature_select_method, n = n_select, train_idx = train_indices)
-            except MemoryError as mem_err:
-                logging.error(f"!! MemoryError encountered while select_feature of {current_loop}: {mem_err}")
-                continue  
+                X_selected, perf_metric_select = select_feature(X = X, y = y, method = feature_select_method, n = n_select, train_idx = train_indices) 
             except Exception as e:
-                logging.error(f"!! An unexpected error occurred while select_feature of {current_loop}: {str(e)}")
+                logging.error(f"An unexpected error occurred while select_feature of {current_loop}. {e.__class__.__name__}: {str(e)}")
                 continue 
 
             logging.info(f" - '{feature_select_method}' feature selection selected {n_select} variants. X_selected.shape = {X_selected.shape}. perf_metrics_selection: {perf_metric_select}")
 
+            try:
+                save_file_prefix = os.path.join(save_data_path, f"{feature_select_method}_{n_select}")
+                draw_PCA(X = X_selected, y = y_original, file_name=save_file_prefix)
+                # draw_tSNE(X = X_selected, y = y_original, file_name=save_file_prefix)
+            except Exception as e:
+                logging.error(f"An unexpected error occurred while draw_PCA or draw_tSNE of {current_loop}. {e.__class__.__name__}: {str(e)}")
+
             X_train, X_test = X_selected[train_indices], X_selected[test_indices]
             y_train, y_test = y[train_indices], y[test_indices]
 
-            for train_model in ["SVM", "XGB", "DT"]:
+            for train_model in ML_models:
                 current_loop["train_model"] = train_model
 
                 logging.info(f" - Start {train_model} training: X_train.shape = {X_train.shape} X_test.shape = {X_test.shape} ")
 
                 try:
-                    y_pred, perf_metric_train = train_ML(method = train_model, X_train = X_train, y_train = y_train, X_test = X_test)
-                except MemoryError as mem_err:
-                    logging.error(f"!! MemoryError encountered while train_ML of {current_loop}: {mem_err}")
-                    continue  
+                    y_pred, perf_metric_train = train_ML(method = train_model, X_train = X_train, y_train = y_train, X_test = X_test) 
                 except Exception as e:
-                    logging.error(f"!! An unexpected error occurred while train_ML of {current_loop}: {str(e)}")
+                    logging.error(f"An unexpected error occurred while train_ML of {current_loop}. {e.__class__.__name__}: {str(e)}")
                     continue 
-                eval_metrics = evaluate_performance(y_test, y_pred)
+                eval_metrics = evaluate_performance(y_test, y_pred, label_mapping, os.path.join(save_data_path, f"{feature_select_method}_{n_select}_{train_model}"))
                 logging.info(f' - Train done with Accuracy: {eval_metrics["accuracy"]*100:.4f}%, perf_metrics_train: {perf_metric_train}')
 
 
@@ -369,11 +376,10 @@ def main():
                                 **{f"{k}": v for k, v in eval_metrics.items() if k != 'confusion_matrix'}}
                 result_combined.append(merged_metrics)
 
+                ## update the dataframe
+                results_df = pd.DataFrame(result_combined)
+                results_df.to_excel(os.path.join(save_data_path, "results.xlsx"), index = False)
 
-    results_df = pd.DataFrame(result_combined)
-    print("--------------- Results ---------------")
-    print(results_df)
-    results_df.to_excel("result.xlsx")
 
 
 if __name__ == "__main__":
